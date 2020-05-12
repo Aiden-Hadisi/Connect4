292c231bf46d1b01f5c5f21ece82f95a9a47c9bd
Aiden Hadisi <so.hadisi@gmail.com>
2020-05-11 20:05:43 -0700

    initial

---
+++ Connect4/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Connect4
+//
+//  Created by CSUFTitan on 5/11/20.
+//  Copyright © 2020 Aiden Hadisi. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+}
