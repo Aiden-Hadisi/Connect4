c0f39c2af6194f410b65c0f7a4a6a6a7a5a8cd17
Aiden Hadisi <so.hadisi@gmail.com>
2020-05-11 20:05:47 -0700

    initial

--- Connect4/Player.swift
+++ Connect4/Player.swift
@@ -7,7 +7,36 @@
 //
 
 import UIKit
+import GameplayKit
+class Player: NSObject, GKGameModelPlayer {
+    var chip: ChipColor
+    var color: UIColor
+    var name: String
+    var playerId: Int
+    
+    var opponent: Player {
+        if chip == .red {
+            return Player.allPlayers[1]
+        } else {
+            return Player.allPlayers[0]
+        }
+    }
 
-class Player: NSObject {
+    static var allPlayers = [Player(chip: .red), Player(chip: .black)]
+    
+    init(chip: ChipColor) {
+        self.chip = chip
+        self.playerId = chip.rawValue
+
+        if chip == .red {
+            color = .red
+            name = "Red"
+        } else {
+            color = .black
+            name = "Black"
+        }
+
+        super.init()
+    }
 
 }
